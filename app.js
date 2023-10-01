642609fd288f1a6520842050b91280b00dc31b1f
faizyi <faiz557564@gmail.com>
2023-10-01 10:05:06 +0500

    work done

---
+++ app.js
@@ -0,0 +1,27 @@
+//Palindrome
+
+// var value = prompt('enter value')
+// var conVal = value.toString().split('').reverse().join('')
+// var strVal = value.toString()
+
+
+// if(strVal == conVal){
+//     alert('Palindrome')
+
+// }
+// else{
+//     alert('Not Palindrome')
+// }
+
+
+
+
+
+//Factorial
+
+// let value = prompt('Enter Factorial Value')
+// let fac = 1;
+// for(value; value>=1; value--){
+//     fac = fac*value;
+//     document.write(fac, '<br>')
+// }
